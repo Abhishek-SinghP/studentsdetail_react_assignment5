ceff0440bb9beff1a0a888b64c78ad1373f5796b
Abhishek-singh <90602408+Abhishek-SinghP@users.noreply.github.com>
2023-04-12 13:36:17 +0530

    Update Student.js
    Update_Home_screen

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -11,19 +11,19 @@ const Student = () => {
     return (
         <>
             <div className="title-container">
-                <h1 className="title">STUDENT DETAILS</h1>
-                <Link to="/student/addStudent" className="btn">Add Student</Link>
+                <h1 className="title">EMPLOYEE DETAILS</h1>
+                <Link to="/student/addStudent" className="btn">Add Employee</Link>
             </div>
             <div className="table-container">
                 <TableContainer component={Paper}>
                     <Table >
                         <TableHead>
                             <TableRow>
-                                <TableCell align="left" style={{width: 175}}>Name</TableCell>
-                                <TableCell align="center">Age</TableCell>
-                                <TableCell align="center">Course</TableCell>
-                                <TableCell align="center">Batch</TableCell>
-                                <TableCell align="center">Change</TableCell>
+                                <TableCell align="left" style={{width: 175}}>NAME</TableCell>
+                                <TableCell align="center">AGE</TableCell>
+                                <TableCell align="center">DESIGNATION</TableCell>
+                                <TableCell align="center">YEAR OF JOINING</TableCell>
+                                <TableCell align="center">CHANGE</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -34,8 +34,8 @@ const Student = () => {
                                         <TableRow key={ID}>
                                             <TableCell component="th" scope="row">{name}</TableCell>
                                             <TableCell align="center">{age}</TableCell>
-                                            <TableCell align="center">{course}</TableCell>
-                                            <TableCell align="center">{batch}</TableCell>
+                                            <TableCell align="center">{designation}</TableCell>
+                                            <TableCell align="center">{date of  joining}</TableCell>
                                             <TableCell align="center"><Link to={`/student/edit/${ID}`} className="edit-btn"><ModeEditIcon /></Link></TableCell>
                                         </TableRow>
                                     )
